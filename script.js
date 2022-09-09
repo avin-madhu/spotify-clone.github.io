3513f3dbf554296e27b768b3652f22b7f6a8f4b9
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2022-09-10 01:14:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-
-let songs=[
+function fn()
+{
+    let songs=[
     {songName:"Until i found you", filePath:"1.mp3",coverPath:"cover1.png"}
-]
-let songIndex =0;
-let audioElement = new Audio("1.mp3");
-let masterplay = document.getElementById("masterplay");
-let bar=document.getElementById("bar");
+    ]
+    let songIndex =0;
+    let audioElement = new Audio("./1.mp3");
+    let masterplay = document.getElementById("masterplay");
+    
+    let bar=document.getElementById("bar");
+    let progress=0;
+    bar.addEventListener('timeupdate', ()=>{console.log('timeupdate');})
 
-bar.addEventListener('timeupdate', ()=>{console.log('timeupdate');})
-masterplay.addEventListener('click',()=>{
-    if(audioElement.paused||audioElement.currentTime<=0)
-    {
-        audioElement.play();
-    }
-})
+
+    masterplay.addEventListener('click',()=>{
+        if(audioElement.paused||audioElement.currentTime<=0)
+        {
+            audioElement.play();
+            masterplay.classList.remove('fa-play');
+            masterplay.classList.add('fa-pause');
+        }
+        else
+        {
+            audioElement.pause();
+            masterplay.classList.add('fa-play');
+            masterplay.classList.remove('fa-pause');
+        }
+        
+    })
+
+    audioElement.addEventListener('timeupdate',()=>{
+        progress= parseInt((audioElement.currentTime/audioElement.duration)*100);
+        bar.value=progress;
+    })
+   bar.addEventListener('change',()=>{
+
+   })
+}
+window.onload = fn
+//<i class="fa-solid fa-pause"></i>
