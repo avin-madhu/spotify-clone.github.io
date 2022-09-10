c3add9c00ead702a85e20ecb03b1e5ac58b54d75
AadithyaSai <69414327+AadithyaSai@users.noreply.github.com>
2022-09-10 13:17:16 +0530

    progress bar working

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
 function fn()
 {
     let songs=[
-    {songName:"Until i found you", filePath:"1.mp3",coverPath:"cover1.png"}
+    {songName:"Until i found you", filePath:"./1.mp3",coverPath:"cover1.png"}
     ]
     let songIndex =0;
     let audioElement = new Audio("./1.mp3");
     let masterplay = document.getElementById("masterplay");
     
-    let bar=document.getElementById("bar");
+    let bar=document.getElementById("ProgressBar");
     let progress=0;
-    bar.addEventListener('timeupdate', ()=>{console.log('timeupdate');})
-
 
     masterplay.addEventListener('click',()=>{
         if(audioElement.paused||audioElement.currentTime<=0)
         {
             audioElement.play();
-            masterplay.classList.remove('fa-play');
-            masterplay.classList.add('fa-pause');
         }
         else
         {
             audioElement.pause();
-            masterplay.classList.add('fa-play');
-            masterplay.classList.remove('fa-pause');
         }
         
     })
 
-    audioElement.addEventListener('timeupdate',()=>{
-        progress= parseInt((audioElement.currentTime/audioElement.duration)*100);
-        bar.value=progress;
+    audioElement.addEventListener('playing', ()=>{
+        masterplay.classList.remove('fa-play');
+        masterplay.classList.add('fa-pause');
     })
-   bar.addEventListener('change',()=>{
 
-   })
+    audioElement.addEventListener('pause' , ()=>{
+        masterplay.classList.add('fa-play');
+        masterplay.classList.remove('fa-pause');
+    })
+
+    audioElement.addEventListener('timeupdate',()=>{
+        bar.value = (audioElement.currentTime / audioElement.duration) * 1000;
+    })
+
+    bar.addEventListener('change',()=>{
+        audioElement.pause();
+        audioElement.currentTime = audioElement.duration * (bar.value / 1000);
+        audioElement.play();
+    })
 }
 window.onload = fn
 //<i class="fa-solid fa-pause"></i>
